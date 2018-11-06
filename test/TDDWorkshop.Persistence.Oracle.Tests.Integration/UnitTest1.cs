26d335dcb2e61a32afc6863a3d5ad79703975b18
Ashish Vegaraju <a.vegaraju@coolblue.nl>
2018-11-06 01:33:23 +0100

    add persistence integration tests project.

---
+++ test/TDDWorkshop.Persistence.Oracle.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TDDWorkshop.Persistence.Oracle.Tests.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
