dbbafadcb2133cac7f02a3018a909e761b0c2995
Ashish Vegaraju <a.vegaraju@coolblue.nl>
2018-11-07 10:42:45 +0100

    controller implementation basic

---
+++ src/TDDWorkShop.Api/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TDDWorkShop.Api.Requests;
+
+namespace TDDWorkShop.Api.Controllers
+{
+    public class ProductsController
+    {
+        public IActionResult Create(MeasurementsApiRequest measurementsRequest)
+        {
+            return new OkResult();
+        }
+    }
+}
---
+++ test/TDDWorkshop.Api.Tests.Unit/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using TDDWorkShop.Api.Controllers;
+using TDDWorkShop.Api.Requests;
+using Xunit;
+
+namespace TDDWorkshop.Api.Tests.Unit.Controllers
+{
+    public class ProductsControllerTests
+    {
+        [Fact]
+        public void Create_WithAValidInput_ReturnsOk()
+        {
+            var measurementsRequest = new MeasurementsApiRequest();
+            
+
+            var sut = new ProductsController();
+
+            var result = sut.Create(measurementsRequest);
+
+            result.Should().BeEquivalentTo(new OkResult());
+        }
+
+    }
+}
